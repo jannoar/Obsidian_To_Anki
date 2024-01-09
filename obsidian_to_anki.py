9379916ebd21b5a06c4a0a19ea19b0cb65d77227
Jan <orb@t480.speedport.ip>
2024-01-09 02:02:44 +0100

    now possible to process multiple files at once. errors may occur, but works for now. code not polished yet.

--- obsidian_to_anki.py
+++ obsidian_to_anki.py
@@ -12,8 +12,8 @@ class App(tk.Tk):
         self.first_dollar = True
         self.processed_text = ''
         self.processed_filename = 'flashcards_processed.md'
-        # self.files_to_be_processed = []
-        # self.processed_filenames = []
+        self.files_to_be_processed = []
+        self.processed_filenames = []
 
         self.button = ttk.Button(self, text='Click',
                                  command=self.get_filename)
@@ -25,29 +25,32 @@ class App(tk.Tk):
 
     def get_filename(self):
         self.filename = askopenfile().name
-        self.processed_filename = self.filename[:-3] + '_processed.md'
+        self.files_to_be_processed.append(self.filename)
+        self.processed_filenames.append(self.filename[:-3] + '_processed.md')
 
     def edit_file(self):
-        with open(self.filename, 'r') as f:
-            text = f.read()
-            print('Vorher: ', text)
+        for file in range(len(self.files_to_be_processed)):
+            self.processed_text = ''
+            with open(self.files_to_be_processed[file], 'r') as f:
+                text = f.read()
+                print('Vorher: ', text)
 
-            for i in range(len(text)):
-                if text[i] == '$' and (text[i-1]) != '\\':
-                    if self.first_dollar is True:
-                        self.processed_text += r'\('
-                        self.first_dollar = False
+                for i in range(len(text)):
+                    if text[i] == '$' and (text[i-1]) != '\\':
+                        if self.first_dollar is True:
+                            self.processed_text += r'\('
+                            self.first_dollar = False
+                        else:
+                            self.processed_text += r'\)'
+                            self.first_dollar = True
                     else:
-                        self.processed_text += r'\)'
-                        self.first_dollar = True
-                else:
-                    self.processed_text += text[i]
-            print('Nachher: ', self.processed_text)
+                        self.processed_text += text[i]
+                print('Nachher: ', self.processed_text)
 
-        with open(self.processed_filename, 'w') as f:
-            print(f'filename: {self.filename} \n\
-                  processed filename: {self.processed_filename}')
-            f.write(self.processed_text)
+            with open(self.processed_filenames[file], 'w') as f:
+                print(f'filename: {self.files_to_be_processed[file]} \n\
+                      processed filename: {self.processed_filenames[file]}')
+                f.write(self.processed_text)
 
 
 if __name__ == '__main__':
