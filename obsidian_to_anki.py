d520bca1940d39b855faffe91bd87b047e7bea94
Jan <orb@t480.speedport.ip>
2024-01-09 23:58:16 +0100

    now you can select multiple files in the selection window. display of selected files still unpleasent.

--- obsidian_to_anki.py
+++ obsidian_to_anki.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 import tkinter as tk
 from tkinter import ttk
-from tkinter.filedialog import askopenfile
+from tkinter.filedialog import askopenfilenames
 
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.geometry('300x300')
-        self.filename = ''
         self.first_dollar = True
         self.processed_text = ''
         self.processed_filename = 'flashcards_processed.md'
@@ -24,16 +23,19 @@ class App(tk.Tk):
         self.edit_button.pack()
 
     def get_filename(self):
-        self.filename = askopenfile().name
-        self.files_to_be_processed.append(self.filename)
-        self.processed_filenames.append(self.filename[:-3] + '_processed.md')
+        self.files_to_be_processed = askopenfilenames()
+        for fn in self.files_to_be_processed:
+            self.processed_filenames.append(fn[:-3] +
+                                            '_processed.md')
+
+        self.fileList = ttk.Label(self, text=f'{self.files_to_be_processed}')
+        self.fileList.pack()
 
     def edit_file(self):
         for file in range(len(self.files_to_be_processed)):
             self.processed_text = ''
             with open(self.files_to_be_processed[file], 'r') as f:
                 text = f.read()
-                print('Vorher: ', text)
 
                 for i in range(len(text)):
                     if text[i] == '$' and (text[i-1]) != '\\':
@@ -45,11 +47,8 @@ class App(tk.Tk):
                             self.first_dollar = True
                     else:
                         self.processed_text += text[i]
-                print('Nachher: ', self.processed_text)
 
             with open(self.processed_filenames[file], 'w') as f:
-                print(f'filename: {self.files_to_be_processed[file]} \n\
-                      processed filename: {self.processed_filenames[file]}')
                 f.write(self.processed_text)
 
 
